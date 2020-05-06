251ff7d08ed4f4bdc9ad92b070ecce37983f7197
Dakota Szabo <dakotajszabo@gmail.com>
2020-05-06 13:03:40 -0400

    Sending UDP Packets

--- src/guan_lifx.ino
+++ src/guan_lifx.ino
@@ -1,21 +1,5 @@
 #include <stdint.h>
 
-// String macAddrList = "";
-
-//UDP udp;
-
-// setup() runs once, when the device is first turned on.
-void setup() {
-  // Put initialization like pinMode and begin functions here.
-  // udp.begin(/*localPort=*/6969);
-  Serial.begin(9600);
-  Serial.println(WiFi.localIP().toString());
-
-}
-
-#define MSGTYPE_SET_COLOR 102
-#define MSGTYPE_SET_POWER 117
-
 // ARM IS LITTLE ENDIAN!!!!!!111
 #pragma pack(push, 1)
 struct PacketHeader {
@@ -75,11 +59,6 @@ struct SetColorPacket {
 
 #pragma pack(pop)
 
-#define MAX_PACKET_SIZE_BYTES 128
-#define FRAME_SIZE_BYTES 8
-
-bool ON = false;
-
 void print_binary(uint8_t* data, size_t size) {
   for (uint32_t i = 0; i < size; i++) {
     char rep[3];
@@ -90,31 +69,84 @@ void print_binary(uint8_t* data, size_t size) {
   Serial.println();
 }
 
-// loop() runs over and over again, as quickly as it can execute.
-void loop() {
+void PopulateHeader(PacketHeader* header, size_t size) {
+  memset(header, 0, sizeof(*header));
+  header->size = size;
+  header->protocol = 1024;
+  header->addressable = 1;
+  header->tagged = 1;
+  header->origin = 0;
+  header->source = 0x69696969;
+}
 
+UDP udp;
+bool ON = false;
+
+#define MSGTYPE_SET_COLOR 102
+#define MSGTYPE_SET_POWER 117
+
+#define LIFX_PORT 56700
+#define LOCAL_PORT LIFX_PORT
+#define REMOTE_PORT LIFX_PORT
+
+void SetPower(bool on) {
+  SetPowerPacket packet;
+  memset(&packet, 0, sizeof(packet));
+  PopulateHeader(&packet.header, sizeof(packet));
+  packet.header.type = MSGTYPE_SET_POWER;
+  packet.level = on ? UINT16_MAX : 0;
+  packet.duration = 3000;
+  SendPacket((uint8_t*) &packet, sizeof(packet));
+}
+
+void SetColor(uint16_t hue, uint16_t sat, uint16_t brt, uint16_t kelvin) {
   SetColorPacket packet;
   memset(&packet, 0, sizeof(packet));
-  
-  packet.header.protocol = 1024;
-  packet.header.addressable = 1;
-  packet.header.tagged = 1;
-  packet.header.origin = 0;
-  packet.header.source = 0x69696969;
+  PopulateHeader(&packet.header, sizeof(packet));
   packet.header.type = MSGTYPE_SET_COLOR;
 
-  packet.color.hue = 21845;
-  packet.color.saturation = 65535;
-  packet.color.brightness = 65535;
-  packet.color.kelvin = 3500;
+  packet.color.hue = hue;
+  packet.color.saturation = sat;
+  packet.color.brightness = brt;
+  packet.color.kelvin = kelvin;
   packet.duration = 1024;
+  SendPacket((uint8_t*) &packet, sizeof(packet));
+}
+
+void SendPacket(uint8_t* packet, size_t size) {
+  Serial.print("Sending Packet from ");
+  Serial.print(WiFi.localIP().toString());
+  Serial.print(":");
+  Serial.print(LIFX_PORT);
+  
+  Serial.print(" to ");
+  Serial.print(BroadcastIP().toString());
+  Serial.print(":");
+  Serial.println(REMOTE_PORT);
+  print_binary(packet, size);
+
+  int result = udp.sendPacket(packet, size, BroadcastIP(), REMOTE_PORT);
+  if (result < 0) {
+    Particle.publish("UDP Send Error", PRIVATE);
+  }
+}
+
+IPAddress BroadcastIP() {
+  const uint32_t mask = ~WiFi.subnetMask().raw().ipv4;
+  const uint32_t bcast = WiFi.localIP().raw().ipv4 | mask;
+  return IPAddress(bcast);
+}
+
+void setup() {
+  udp.begin(/*localPort=*/LOCAL_PORT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  //SetColor(21845, UINT16_MAX, UINT16_MAX, 3500);
   ON = !ON;
-  // packet.level = ON ? 65535 : 0;
+  SetPower(ON);
 
-  packet.header.size = sizeof(packet);
-
-  print_binary((uint8_t*) &packet, sizeof(packet));
-
-  delay(4000);
+  delay(3000);
   
 }
